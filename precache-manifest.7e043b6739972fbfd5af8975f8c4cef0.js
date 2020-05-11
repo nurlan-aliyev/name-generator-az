d768d77f4810bbc4b028154664df48ac3e798fe0
unknown <nurlan.aliyev58@gmail.com>
2020-05-11 20:32:59 +0400

    Updates

---
+++ precache-manifest.7e043b6739972fbfd5af8975f8c4cef0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b447b2c79a9af8cc801e935b3a1c410b",
+    "url": "/name-generator-az/index.html"
+  },
+  {
+    "revision": "6d42ddaaceaadecf8cc7",
+    "url": "/name-generator-az/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/name-generator-az/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/name-generator-az/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d42ddaaceaadecf8cc7",
+    "url": "/name-generator-az/static/js/main.2fc9a6c9.chunk.js"
+  },
+  {
+    "revision": "62685539b652b7728245",
+    "url": "/name-generator-az/static/js/runtime-main.6560116f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/name-generator-az/static/media/logo.5d5d9eef.svg"
+  }
+]);
